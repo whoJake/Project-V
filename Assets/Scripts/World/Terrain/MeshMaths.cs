58c286b1d5ea556798fe76aac37d93669b0ef872
Jake Cooper <37589250+whoJake@users.noreply.github.com>
2023-05-07 18:22:58 +0100

    Added RemoveDuplicateVertices

--- Assets/Scripts/World/Terrain/MeshMaths.cs
+++ Assets/Scripts/World/Terrain/MeshMaths.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class MeshMaths
+namespace MeshMaths
 {
     public class MeshInfo {
         public MeshInfo(Vector3[] _vertices, int[] _triangles) {
@@ -26,72 +26,108 @@ public static class MeshMaths
         public Vector3 c;
     }
 
-    public static MeshInfo SubdivideTriangle(Triangle tri, int divisions) {
-        Debug.Assert(divisions >= 1, "Cannot subdivide a triangle less than once");
+    public static class MeshMaths {
 
-        int maxDepth = divisions + 2;
+        //
+        // Summery:
+        //      Subdivide a triangle with each side having divisions more points added to it
+        //
+        public static MeshInfo SubdivideTriangle(Triangle tri, int divisions) {
+            Debug.Assert(divisions >= 1, "Cannot subdivide a triangle less than once");
 
-        Vector3[] leftPoints = SubdivideEdge(tri.a, tri.b, maxDepth);
-        Vector3[] rightPoints = SubdivideEdge(tri.a, tri.c, maxDepth);
+            int maxDepth = divisions + 2;
 
-        int verticesLength = (maxDepth * (maxDepth + 1)) / 2;
-        Vector3[] vertices = new Vector3[verticesLength];
-        vertices[0] = tri.a;
-        int vertexIndexCount = 1;
+            Vector3[] leftPoints = SubdivideEdge(tri.a, tri.b, maxDepth);
+            Vector3[] rightPoints = SubdivideEdge(tri.a, tri.c, maxDepth);
 
-        List<int> triangles = new List<int>();
+            int verticesLength = (maxDepth * (maxDepth + 1)) / 2;
+            Vector3[] vertices = new Vector3[verticesLength];
+            vertices[0] = tri.a;
+            int vertexIndexCount = 1;
 
-        for(int depth = 1; depth < maxDepth; depth++) {
-            //Add Vertices
-            Vector3 left = leftPoints[depth];
-            Vector3 right = rightPoints[depth];
+            List<int> triangles = new List<int>();
 
-            Vector3[] depthPoints = SubdivideEdge(left, right, depth + 1);
-            for (int i = 0; i < depthPoints.Length; i++) {
-                vertices[vertexIndexCount] = depthPoints[i];
-                vertexIndexCount++;
+            for (int depth = 1; depth < maxDepth; depth++) {
+                //Add Vertices
+                Vector3 left = leftPoints[depth];
+                Vector3 right = rightPoints[depth];
+
+                Vector3[] depthPoints = SubdivideEdge(left, right, depth + 1);
+                for (int i = 0; i < depthPoints.Length; i++) {
+                    vertices[vertexIndexCount] = depthPoints[i];
+                    vertexIndexCount++;
+                }
+
+                //Add Triangles
+                int leftIndex = (depth * (depth + 1)) / 2;
+                int rightIndex = leftIndex + depth;
+
+                int aboveLeftIndex = (depth - 1) * depth / 2;
+                int aboveRightIndex = aboveLeftIndex + (depth - 1);
+
+                //Upwards pointing triangles
+                for (int i = aboveLeftIndex; i <= aboveRightIndex; i++) {
+                    int a = i;
+                    int b = depth + i;
+                    int c = b + 1;
+
+                    triangles.AddRange(new int[3] { a, b, c });
+                }
+
+                //Downwards pointing triangles
+                for (int i = leftIndex + 1; i <= rightIndex - 1; i++) {
+                    int a = i;
+                    int c = i - depth - 1;
+                    int b = c + 1;
+
+                    triangles.AddRange(new int[3] { a, b, c });
+                }
             }
 
-            //Add Triangles
-            int leftIndex = (depth * (depth + 1)) / 2;
-            int rightIndex = leftIndex + depth;
-
-            int aboveLeftIndex = (depth - 1) * depth / 2;
-            int aboveRightIndex = aboveLeftIndex + (depth - 1);
-
-            //Upwards pointing triangles
-            for(int i = aboveLeftIndex; i <= aboveRightIndex; i++) {
-                int a = i;
-                int b = depth + i;
-                int c = b + 1;
-
-                triangles.AddRange(new int[3] { a, b, c });
-            }
-
-            //Downwards pointing triangles
-            for(int i = leftIndex + 1; i <= rightIndex - 1; i++) {
-                int a = i;
-                int c = i - depth - 1;
-                int b = c + 1;
-
-                triangles.AddRange(new int[3] { a, b, c });
-            }
+            return new MeshInfo(vertices, triangles.ToArray());
         }
 
-        return new MeshInfo(vertices, triangles.ToArray());
-    }
+        //
+        // Summery:
+        //      Subdivide and return the interpolated points between a and b
+        //
+        public static Vector3[] SubdivideEdge(Vector3 a, Vector3 b, int vertsOnNewEdge) {
+            Debug.Assert(vertsOnNewEdge >= 2, "Cannot subdivide an edge to have less than 2 vertices");
 
-    public static Vector3[] SubdivideEdge(Vector3 a, Vector3 b, int vertsOnNewEdge) {
-        Debug.Assert(vertsOnNewEdge >= 2, "Cannot subdivide an edge to have less than 2 vertices");
+            Vector3[] result = new Vector3[vertsOnNewEdge];
+            Vector3 a2b = b - a;
+            Vector3 step = a2b / (vertsOnNewEdge - 1);
 
-        Vector3[] result = new Vector3[vertsOnNewEdge];
-        Vector3 a2b = b - a;
-        Vector3 step = a2b / (vertsOnNewEdge - 1);
-
-        for(int i = 0; i < vertsOnNewEdge; i++) {
-            result[i] = a + (step * i);
+            for (int i = 0; i < vertsOnNewEdge; i++) {
+                result[i] = a + (step * i);
+            }
+            return result;
         }
-        return result;
-    }
 
+        //
+        // Summery:
+        //      Remove duplicate vertices from a MeshInfo whilst correctly maintaining the triangles
+        //
+        public static void RemoveDuplicateVertices(MeshInfo mesh) {
+            Dictionary<Vector3, int> vertToIndex = new Dictionary<Vector3, int>();
+            List<Vector3> vertices = new List<Vector3>();
+            int[] triangles = new int[mesh.triangles.Length];
+
+            for(int i = 0; i < mesh.triangles.Length; i++) {
+                int vertexIndex = mesh.triangles[i];
+                Vector3 vertex = mesh.vertices[vertexIndex];
+                if (vertToIndex.ContainsKey(vertex)) {
+                    triangles[i] = vertToIndex[vertex];
+                } else {
+                    vertexIndex = vertices.Count;
+                    vertices.Add(vertex);
+                    triangles[i] = vertexIndex;
+                    vertToIndex.Add(vertex, vertexIndex);
+                }
+            }
+            Debug.Log("Removed " + (mesh.vertices.Length - vertices.Count) + " vertices");
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles;
+        }
+    }
 }
